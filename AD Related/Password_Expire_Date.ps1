62d8921f8a1482d98f5f9a163303e90ba80f346b
lookismashed <156612654+cameronm77@users.noreply.github.com>
2024-08-20 21:53:27 -0500

    Create Password_Expire_Date.ps1

---
+++ AD Related/Password_Expire_Date.ps1
@@ -0,0 +1,3 @@
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" |
+
+Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Export-Csv d:\pwdexpiry.csv
