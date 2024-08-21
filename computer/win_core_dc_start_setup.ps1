669fb28f4984f7c7d87e1d2ffd561f80064a498c
lookismashed <156612654+cameronm77@users.noreply.github.com>
2024-08-20 21:22:26 -0500

    Create win_core_dc_start_setup.ps1

---
+++ computer/win_core_dc_start_setup.ps1
@@ -0,0 +1,11 @@
+Get-timezone
+
+Set-TimeZone -Id "Central Standard Time"
+
+Get-WindowsFeature AD-Domain-Services | Install-WindowsFeature Ad-domain-services
+
+
+
+Import-Module ADDSDeplyoment
+
+Install-ADDSForest
